f86f65ffac855ec6b244eaa04330e3f8059da5a5
Ari Ayvazyan <ariay@live.at>
2015-07-19 21:44:47 +0200

    disabled settings

--- app/src/main/java/com/github/aayvazyan/udacitystarter/UdacityStarter.java
+++ app/src/main/java/com/github/aayvazyan/udacitystarter/UdacityStarter.java
@@ -37,7 +37,7 @@ public class UdacityStarter extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_udacity_starter, menu);
-        return true;
+        return false; //Disable the options
     }
 
     @Override
